9dab2b21cb524f1827dfa9c0abc199e0d72071cb
IvanSilvaLi <ivansilira@outlook.com>
2023-04-11 14:10:17 -0300

    Primeiro Commit, Criação dos primeiros elementos

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function Converter() {
+    let valDolar = document.getElementById("Dolar")
+    let valEuro = document.getElementById("Euro")
+    let valLibra = document.getElementById("Libra")
+    let valReal = document.getElementById("Real")
+
+    document.getElementById("resuleuro").innerHTML = valReal + valEuro;
+}
